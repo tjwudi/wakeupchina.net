728552dbcc27e7a60eb5d190685652a34698d7e1
tjwudi <webmaster@leapoahead.com>
2015-12-23 14:04:25 -0800

    Admin backend for Post
    Signed-off-by: tjwudi <webmaster@leapoahead.com>

--- board/admin.py
+++ board/admin.py
@@ -1,2 +1,9 @@
-# from django.contrib import admin
-# Register your models here.
+from django.contrib import admin
+
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
